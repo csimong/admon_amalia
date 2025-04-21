d67468359b6209d5e7a84f85d54dc4951b220d99
csimong <carolina.simon.guerrero@gmail.com>
2025-04-21 20:14:59 +0200

    Add User Manager and first version of main user edit window

--- gui/app.py
+++ gui/app.py
@@ -1,19 +1,64 @@
 """Main window configuration. It creates the main app window."""
 
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QMessageBox, \
+    QHBoxLayout, QVBoxLayout, QListWidget
+from logic.user_manager import UserManager
 
 
 class MyApp(QMainWindow):
-    """Main application window."""
-
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Mi Aplicación PyQt")
-        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle("Mi App de Usuarios")
+        self.setGeometry(100, 100, 600, 300)
 
-        label = QLabel("¡Hola, PyQt!", self)
-        label.move(150, 130)
+        # Initialize User manager
+        self.user_manager = UserManager()
+
+        # Create user name widgets
+        self.label_name = QLabel("Nombre:", self)
+        self.label_name.move(20, 30)
+        self.input_name = QLineEdit(self)
+        self.input_name.setPlaceholderText("Ej: David")
+        self.input_name.move(100, 30)
+
+        # Create user surnames widgets
+        self.label_surnames = QLabel("Apellidos:", self)
+        self.label_surnames.move(20, 70)
+        self.input_surnames = QLineEdit(self)
+        self.input_surnames.setPlaceholderText("Ej: Bisbal")
+        self.input_surnames.move(100, 70)
+
+        # Create user email widgets
+        self.label_email = QLabel("Email:", self)
+        self.label_email.move(20, 110)
+        self.input_email = QLineEdit(self)
+        self.input_email.setPlaceholderText("Ej: davidbisbal@gmail.com")
+        self.input_email.move(100, 110)
+
+        # Create user phone widgets
+        self.label_phone = QLabel("Teléfono:", self)
+        self.label_phone.move(20, 150)
+        self.input_phone = QLineEdit(self)
+        self.input_phone.setPlaceholderText("Ej: +34671345234")
+        self.input_phone.move(100, 150)
+
+        # Save user data in database
+        self.btn_save = QPushButton("Guardar", self)
+        self.btn_save.move(100, 190)
+        self.btn_save.clicked.connect(self.add_user)
+
+    def add_user(self):
+        name = self.input_name.text()
+        surnames = self.input_surnames.text()
+        email = self.input_email.text()
+        phone = self.input_phone.text()
+        if name and surnames:
+            self.user_manager.add_user(name, surnames, email, phone)
+            print("Usuario agregado:", name, surnames, email, phone)
+        else:
+            QMessageBox.warning(
+                self, "Error", "Debes especificar nombre y apellidos del usuario.")
 
 
 if __name__ == "__main__":
--- logic/user_manager.py
+++ logic/user_manager.py
@@ -1,1 +1,22 @@
 """User management file. It contains functions to create, delete and update users."""
+
+from database.database import Database
+
+
+class UserManager():
+    def __init__(self):
+        self.db = Database()
+
+    def add_user(self, name, surnames, email, phone):
+        self.db.execute(
+            "INSERT INTO clientes (nombre, apellidos, email, telefono) VALUES (?, ?, ?, ?)", (name, surnames, email, phone))
+
+    def get_users(self):
+        return self.db.get("SELECT id, nombre, apellidos, email, telefono FROM clientes")
+
+    def close(self):
+        self.db.close()
+
+
+# Create here the concatenated id of lottery table
+# id = f"{numero}{sorteo}{año}"
